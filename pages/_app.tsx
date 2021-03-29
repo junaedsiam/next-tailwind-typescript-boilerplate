2fe7ffd2e64f4cd0bd0dd20b23c55f7f2b09d847
junaedsiam <junaedsiam@gmail.com>
2021-03-29 12:49:20 +0600

    initial broilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/app";
+import 'styles/global.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+
+
+    return <Component {...pageProps} />
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+  <div className="container mx-auto py-8">
+    <div className="max-w-xl border-2 border-gray-200 border-opacity-50 mx-auto p-8 rounded-lg">
+      <h1 className="text-3xl font-light mb-3">Tailwind + NextJS broilerplate with Typescript !</h1>
+      <p className="leading-relaxed text-gray-600 mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente magnam fugiat obcaecati a vitae? Fuga velit quis laborum itaque accusantium.</p>
+      <a href="#" className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 border border-transparent rounded">Sample Button</a>
+    </div>
+  </div>
+);
 
 export default IndexPage
