2e93e19db613999b2fec9668e8d1e3f319e72434
junaedsiam <junaedsiam@gmail.com>
2021-03-29 13:08:02 +0600

    eslint and prettier addition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import { AppProps } from "next/app";
-import 'styles/global.css';
+import { AppProps } from 'next/app'
+import 'styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-
-
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
